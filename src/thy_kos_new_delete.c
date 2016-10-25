24f958cce0eef8163666df88eacf5838f501ddd7
thymios <etzinis@gmail.com>
2016-10-25 17:00:22 +0300

    New and Delete with long converion to string

--- src/thy_kos_new_delete.c
+++ src/thy_kos_new_delete.c
@@ -38,13 +38,28 @@ int insert_to_list (char * Addr){
 	return 0;
 }
 
-int add_to_list (char * Addr){
+int LongToString(long x, char * SpecAddr){
+	const int n = snprintf(NULL, 0, "%lu", x);
+	if (n <= 0) return 1;
+	char buf[n+1];
+	int c = snprintf(buf, n+1, "%lu", x);
+	buf[n] == '\0';
+	if (!(c == n)) return 1;
+	strcpy(SpecAddr, buf);
+	return 0;
+}
+
+int add_to_list (long x){
+	char Addr[80];
+	if (LongToString(x,Addr)) return 1;
 	if (head == NULL) return construct_list(Addr);
 	else return insert_to_list(Addr);
 }
 
 
-int delete_from_list(char * Addr){
+int delete_from_list(long x){
+	char Addr[80];
+	if (LongToString(x,Addr)) return 1;
 	AddrPtr t = head; 
 	if (t==NULL) return -1;
 	if ( strcmp( t->alocated_address, Addr) == 0 ) {
@@ -69,16 +84,16 @@ int delete_from_list(char * Addr){
 
 int main(int argc, char const *argv[])
 { 
-	char a[80];
+	long a;
 	newest_element = (AddrPtr) malloc(sizeof(address));
 	head = (AddrPtr) malloc(sizeof(address));
 	newest_element = NULL;
 	head = NULL;
 	int dummy;
 
-	while ( strcmp(a, "q\0") != 0){
+	while ( a != 0){
 		printf("Dwse mia dieythinsi na eisagw\n");
-		scanf("%s",&a);
+		scanf("%ld",&a);
 		dummy = add_to_list(a);
 		//printf("%s\n", a);
 		for(AddrPtr t = head; t!=NULL; t=t->next){
@@ -87,9 +102,9 @@ int main(int argc, char const *argv[])
 		printf("\n");
 	}
 
-	while ( strcmp(a, "w\0") != 0){
+	while ( a != 0){
 		printf("Dwse mia dieythinsi na diagrapsw\n");
-		scanf("%s",&a);
+		scanf("%ld",&a);
 		dummy = delete_from_list(a);
 		//printf("%s\n", a);
 		for(AddrPtr t = head; t!=NULL; t=t->next){
